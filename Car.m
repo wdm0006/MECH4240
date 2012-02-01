2fbf965ca6533b15c0744b5a7dcc90c2aeabdd16
Will McGinnis <wdm0006@auburn.edu>
2012-01-31 22:41:39 -0600

    Car object simplified significantly.

--- Car.m
+++ Car.m
@@ -3,22 +3,25 @@
 % conditions and inputs allow various simulations, and get functions allow 
 % the monitoring and analysis of the system's response. 
 
+%Version 2
+% -Added in a braking force input to simulation, negative is propulsive
+% -Deprecated coasting sim, just use plain sim with no input
+% -Added in terrain map ability, road grade integrated into that,
+% deprecated as a seperate parameter
+% -Seperated Constructor and simulation setup
+% -Terminal Velocity property deprecated, user inputs Cd and Af
+
 classdef Car < handle
     
 %% Class Properties    
     properties 
         %System Properties: the parameters which define the car itself.
-        %Road Grade: Local to the car, tied to X coordinate axis
-        road_Grade
         %Mass: kg
         mass
         %Gravity is assumed to be 9.81 m/s
         g=9.81;
         %Drag Coefficient
         Cd
-        %Terminal Velocity is used to estimate the drag coefficient if 
-        %one is not given.
-        mv 
         %Width of the wheels of the car
         width
         %Wheel to wheel length of car
@@ -31,6 +34,8 @@ classdef Car < handle
         cg_height
         %Frontal Area
         fa
+        
+        
         %Parameters for the simulation of the system
         x
         xdot
@@ -52,14 +57,37 @@ classdef Car < handle
     %% Methods
     methods
         %% Constructor
-        %Sets initial conditions and intializes the simulation parameters.
-        function obj=Car(rg, m, terminalv, xdot0,ydot0, d, yawrate, wbw, wbl,rf,crr,cgh, Af)
-            obj.road_Grade=rg;
+        %Sets parameters of the car itself
+        function obj=Car(m,wbw, wbl,rf,crr,cgh, Af,d)
             obj.mass=m;
-            obj.mv=terminalv;
             obj.del=d;
             obj.length=wbl;
             obj.width=wbw;
+            obj.r_eff=rf;
+            obj.C_rr=crr;
+            obj.cg_height=cgh;
+            obj.fa=Af;
+            obj.Cd=0;
+            
+            %simulation parameters set to 0 just in case
+            obj.x=0;
+            obj.xdot=0;
+            obj.xdotnaught=0;
+            obj.xdoubledot=0;           
+            obj.y=0;
+            obj.ydot=0;
+            obj.ydoubledot=0;
+            obj.ydotnaught=0;
+            obj.yaw=0;
+            obj.yawdot=0;
+            obj.yawdotnaught=0;
+            obj.N=0;
+            obj.D=0;
+            obj.E=0;
+        end
+        %% Setup
+        %sets the initial conditions of the simulation
+        function obj=SetupSim(obj,xdot0,ydot0, yawrate )
             obj.x=0;
             obj.xdot=xdot0;
             obj.xdotnaught=xdot0;
@@ -74,86 +102,15 @@ classdef Car < handle
             obj.N=0;
             obj.D=0;
             obj.E=0;
-            obj.r_eff=rf;
-            obj.C_rr=crr;
-            obj.cg_height=cgh;
-            obj.fa=Af;
         end
-        %% Step Coasting Simulation
-        %steps forward object in time one time_step, using the
-        %aerodynamic drag model designated by order.  If order is set
-        %to -1, aerodynamic drag is neglected completely.  Global
-        %coordinate vectors are also updated.
-        %note:does not yet support yaw double dot term.  Uses known
-        %steady y dot to turn the car. Also does not include rolling
-        %resistance losses
-        %Negative road grade is assumed downhill
-        function out=stepCoastSim(obj,time_step,order)
-            
-            [vn1, ve1]=obj.getGlobalVelocity;
-            if order==-1
-                obj.xdoubledot(end+1)=-obj.g*sin(obj.road_Grade);
-                obj.xdot(end+1)=obj.xdot(end)+(.5*time_step*(obj.xdoubledot(end)+obj.xdoubledot(end-1)));
-                obj.x(end+1)=obj.x(end)+(.5*time_step*(obj.xdot(end)+obj.xdot(end-1)));
-                obj.ydoubledot(end+1)=0;
-                obj.ydot(end+1)=obj.ydot(end);
-                obj.y(end+1)=obj.y(end)+(.5*time_step*(obj.ydot(end)+obj.ydot(end)));
-                obj.yaw(end+1)=obj.yaw(end)+(.5*time_step*(obj.yawdot+obj.yawdot));
-            else
-                obj.xdoubledot(end+1)=(-obj.mass*obj.g*sin(obj.road_Grade)-(obj.Cd*obj.xdot(end)^order))/obj.mass;
-                obj.xdot(end+1)=obj.xdot(end)+(.5*time_step*(obj.xdoubledot(end)+obj.xdoubledot(end-1)));
-                obj.x(end+1)=obj.x(end)+(.5*time_step*(obj.xdot(end)+obj.xdot(end-1)));
-                obj.ydoubledot(end+1)=0;
-                obj.ydot(end+1)=obj.ydotnaught;
-                obj.y(end+1)=obj.y(end)+(.5*time_step*(obj.ydot(end)+obj.ydot(end-1)));
-                obj.yaw(end+1)=obj.yaw(end)+(.5*time_step*(obj.yawdot+obj.yawdot));
-            end
-            [vn2, ve2]=obj.getGlobalVelocity;
-            obj.N(end+1)=obj.N(end)+(time_step*.5*(vn1+vn2));
-            obj.E(end+1)=obj.E(end)+(time_step*.5*(ve1+ve2));
-            obj.D(end+1)=obj.D(end)+((obj.x(end)-obj.x(end-1))*sin(obj.road_Grade));
-            out=1;
-        end
-        %% Step Forward Braking Simulation
+        %% Step Forward Terrain-Braking Simulation
         %Functionally the same as the coasting simulation, but with a
         %parameter for force applied by braking.  Because of the simplicity
         %of the model at this point, a negative braking force input would
         %simulate propulsion.  Does support rolling resistance, does not
         %support yaw double dot.
         %negative road grade is downhill.
-        function out=stepBrakeSim(obj,time_step,order,F_brake)
-                [vn1, ve1]=obj.getGlobalVelocity;
-                if order==-1
-                    obj.xdoubledot(end+1)=-obj.g*sin(obj.road_Grade)-(F_brake/obj.mass);
-                    obj.xdot(end+1)=obj.xdot(end)+(.5*time_step*(obj.xdoubledot(end)+obj.xdoubledot(end-1)));
-                    obj.x(end+1)=obj.x(end)+(.5*time_step*(obj.xdot(end)+obj.xdot(end-1)));
-                    obj.ydoubledot(end+1)=0;
-                    obj.ydot(end+1)=obj.ydot(end);
-                    obj.y(end+1)=obj.y(end)+(.5*time_step*(obj.ydot(end)+obj.ydot(end)));
-                    obj.yaw(end+1)=obj.yaw(end)+(.5*time_step*(obj.yawdot+obj.yawdot));
-                else
-                    obj.xdoubledot(end+1)=-obj.g*sin(obj.road_Grade)-((obj.Cd*obj.fa*(obj.xdot(end)^order))/obj.mass)-(F_brake/obj.mass)-(obj.g*obj.C_rr);
-                    obj.xdot(end+1)=obj.xdot(end)+(.5*time_step*(obj.xdoubledot(end)+obj.xdoubledot(end-1)));
-                    obj.x(end+1)=obj.x(end)+(.5*time_step*(obj.xdot(end)+obj.xdot(end-1)));
-                    obj.ydoubledot(end+1)=0;
-                    obj.ydot(end+1)=obj.ydotnaught;
-                    obj.y(end+1)=obj.y(end)+(.5*time_step*(obj.ydot(end)+obj.ydot(end-1)));
-                    obj.yaw(end+1)=obj.yaw(end)+(.5*time_step*(obj.yawdot+obj.yawdot));
-                end
-                [vn2, ve2]=obj.getGlobalVelocity;
-                obj.N(end+1)=obj.N(end)+(time_step*.5*(vn1+vn2));
-                obj.E(end+1)=obj.E(end)+(time_step*.5*(ve1+ve2));
-                obj.D(end+1)=obj.D(end)+((obj.x(end)-obj.x(end-1))*sin(obj.road_Grade));
-                out=1;
-        end
-        %% Step Forward Terrain Braking Simulation
-        %Functionally the same as the coasting simulation, but with a
-        %parameter for force applied by braking.  Because of the simplicity
-        %of the model at this point, a negative braking force input would
-        %simulate propulsion.  Does support rolling resistance, does not
-        %support yaw double dot.
-        %negative road grade is downhill.
-        function out=stepTerrainBrakeSim(obj,time_step,order,F_brake)
+        function out=stepSim(obj,time_step,order,F_brake)
                 [vn1, ve1]=obj.getGlobalVelocity;
                 gradient=obj.getLocalGradient;
                 if order==-1
@@ -176,7 +133,11 @@ classdef Car < handle
                 [vn2, ve2]=obj.getGlobalVelocity;
                 obj.N(end+1)=obj.N(end)+(time_step*.5*(vn1+vn2));
                 obj.E(end+1)=obj.E(end)+(time_step*.5*(ve1+ve2));
-                obj.D(end+1)=obj.terrain(ceil(obj.N(end)+(size(obj.terrain,1)/2)+.5), ceil(obj.E(end)+(size(obj.terrain,2)/2)+.5));
+                if size(obj.terrain,1)==1 && size(obj.terrain,2)==1
+                    obj.D(end+1)=obj.D(end)+(sin(obj.terrain)*(obj.x(end)-obj.x(end-1)));
+                else
+                    obj.D(end+1)=obj.terrain(ceil(obj.N(end)+(size(obj.terrain,1)/2)+.5), ceil(obj.E(end)+(size(obj.terrain,2)/2)+.5));
+                end
                 out=1;
         end
         %% Calculate Tire Velocities
@@ -212,9 +173,8 @@ classdef Car < handle
         function out=simulateCoastingModels(obj, time_step,order)
             timevec=0;
             obj.resetValues;
-            obj.Cd=(obj.mass*obj.g*sin(obj.road_Grade))/(obj.mv^order);
             while obj.x(end)<250 
-                obj.stepCoastSim(time_step,order);
+                obj.stepSim(time_step,order,0);
                 timevec(end+1)=timevec(end)+time_step;
             end
             out=horzcat(timevec',obj.x', obj.xdot');
@@ -223,9 +183,9 @@ classdef Car < handle
         %Helper function to set the drag coefficient.
         function out=setCD(obj,varargin)
             if isempty(varargin)
-                obj.Cd=(obj.mass*obj.g*sin(obj.road_Grade))/(obj.mv^2);
+                obj.Cd=0;
             else
-            obj.Cd=varargin{1,1};
+                obj.Cd=varargin{1,1};
             end
             out=obj.Cd;
         end
@@ -271,6 +231,9 @@ classdef Car < handle
         %as the center point is the dimension (0,0,0) in NED global
         %coorinates.  Function normalizes the Down values to the center
         %being 0.
+        
+        %if the size of the matrix is one, the value is assumed to be a
+        %localized gradient in radians
         function out=setPlanarLandscape(obj, inMat)
             if(mod(size(inMat, 1),2)~=1)
                 out=0;
@@ -282,39 +245,47 @@ classdef Car < handle
                 obj.terrain=inMat;
                 out=1;
             end
-            obj.D=inMat(floor(size(inMat,1)/2), floor(size(inMat,2)/2));
+            if size(obj.terrain,1)==1 && size(obj.terrain,2)==1
+                obj.D=0;
+            else
+                obj.D=inMat(floor(size(inMat,1)/2), floor(size(inMat,2)/2));
+            end
         end
         %% Get Local Grandient
         %uses the global position and velocity to determine current
         %location on the terrain map, as well as the likely next position,
-        %and exrapolates a local gradient from that. 
+        %and exrapolates a local gradient from that.
         function out=getLocalGradient(obj)
-            [vn,ve,n,e,~]=obj.getGlobalState;
-            n=n+((size(obj.terrain,1)/2)+.5);
-            e=e+((size(obj.terrain,2)/2)+.5);
-            
-            curr_elevation=obj.terrain(ceil(n), ceil(e));
-            theta=atan(vn/ve);
-            if (theta>=0 && theta<=pi/8) || (theta<=0 && theta>=(15*pi)/8)
-                next_elevation=obj.terrain(ceil(n), ceil(e)+1);
-            elseif (theta>pi/8) && (theta<=(3*pi)/8)
-                next_elevation=obj.terrain(ceil(n)+1, ceil(e)+1);
-            elseif (theta>(3*pi)/8) && (theta<=(5*pi)/8)
-                next_elevation=obj.terrain(ceil(n)+1, ceil(e));
-            elseif (theta>(5*pi)/8) && (theta<=(7*pi)/8)
-                next_elevation=obj.terrain(ceil(n)+1, ceil(e)-1);
-            elseif (theta>(7*pi)/8) && (theta<=(9*pi)/8)
-                next_elevation=obj.terrain(ceil(n), ceil(e)-1);
-            elseif (theta>(9*pi)/8) && (theta<=(11*pi)/8)
-                next_elevation=obj.terrain(ceil(n)-1, ceil(e)-1);
-            elseif (theta>(11*pi)/8) && (theta<=(13*pi)/8)
-                next_elevation=obj.terrain(ceil(n)-1, ceil(e));
-            else 
-                next_elevation=obj.terrain(ceil(n)-1, ceil(e)+1);
+            if size(obj.terrain,1)==1 && size(obj.terrain,2)==1
+                out=obj.terrain;
+            else
+                [vn,ve,n,e,~]=obj.getGlobalState;
+                n=n+((size(obj.terrain,1)/2)+.5);
+                e=e+((size(obj.terrain,2)/2)+.5);
+                
+                curr_elevation=obj.terrain(ceil(n), ceil(e));
+                theta=atan(vn/ve);
+                if (theta>=0 && theta<=pi/8) || (theta<=0 && theta>=(15*pi)/8)
+                    next_elevation=obj.terrain(ceil(n), ceil(e)+1);
+                elseif (theta>pi/8) && (theta<=(3*pi)/8)
+                    next_elevation=obj.terrain(ceil(n)+1, ceil(e)+1);
+                elseif (theta>(3*pi)/8) && (theta<=(5*pi)/8)
+                    next_elevation=obj.terrain(ceil(n)+1, ceil(e));
+                elseif (theta>(5*pi)/8) && (theta<=(7*pi)/8)
+                    next_elevation=obj.terrain(ceil(n)+1, ceil(e)-1);
+                elseif (theta>(7*pi)/8) && (theta<=(9*pi)/8)
+                    next_elevation=obj.terrain(ceil(n), ceil(e)-1);
+                elseif (theta>(9*pi)/8) && (theta<=(11*pi)/8)
+                    next_elevation=obj.terrain(ceil(n)-1, ceil(e)-1);
+                elseif (theta>(11*pi)/8) && (theta<=(13*pi)/8)
+                    next_elevation=obj.terrain(ceil(n)-1, ceil(e));
+                else
+                    next_elevation=obj.terrain(ceil(n)-1, ceil(e)+1);
+                end
+                %if each entry is one meter;
+                dist=1;
+                out=atan((next_elevation-curr_elevation)/(dist));
             end
-            %if each entry is one meter;
-            dist=1;
-            out=atan((next_elevation-curr_elevation)/(dist));
         end
     end
     
--- testscript.m
+++ testscript.m
@@ -1,46 +1,43 @@
 clear all; clc;
-%% Simulating a local gradient accelleration, helix
-% road_grade=-(20/360)*2*pi;
-% mass=1500; 
-% r_eff=.35; 
-% terminal_velocity=140*.44704;
-% xdot_naught=20;
-% ydot_naught=1;
-% del=(5/360)*2*pi;
-% yaw_rate=(50/360)*2*pi;
-% wheel_base_width=1.5;
-% wheel_base_length=2.5;
-% c_rr=0.01;
-% cg_height=.5;
-% frontal_area=2;
-% 
-% %initializing generic car model
-% model=Car(road_grade,mass,terminal_velocity,xdot_naught,ydot_naught,...
-%             del,yaw_rate,wheel_base_width,wheel_base_length,r_eff,c_rr,cg_height,frontal_area);
-% 
-% x=0;
-% xdot=xdot_naught;
-% yaw=0;
-% y=0;
-% ydot=ydot_naught;
-% n=0;
-% e=0;
-% d=0;
-% 
-% F_brake=-100;
-% 
-% model.setCD(.3);
-% for j=1:1000;
-%     model.stepBrakeSim(.01, 2, F_brake);
-%     [yaw(end+1), x(end+1),xdot(end+1),~,y(end+1),ydot(end+1),~]=model.getState;
-%     [~,~,n(end+1),e(end+1),d(end+1)]=model.getGlobalState;
-% end
-% 
-% figure; 
-% plot3(n, e, d);
-% xlabel('NORTH');
-% ylabel('EAST');
-% zlabel('DOWN');
+% Simulating a local gradient accelleration, helix
+road_grade=-(20/360)*2*pi;
+mass=1500; 
+r_eff=.35; 
+terminal_velocity=140*.44704;
+xdot_naught=20;
+ydot_naught=1;
+del=(5/360)*2*pi;
+yaw_rate=(50/360)*2*pi;
+wheel_base_width=1.5;
+wheel_base_length=2.5;
+c_rr=0.01;
+cg_height=.5;
+frontal_area=2;
+
+
+%initializing generic car model
+model=Car(mass, wheel_base_width,wheel_base_length,r_eff,c_rr,cg_height,frontal_area,del);
+%setting up a simulation
+model=model.SetupSim(xdot_naught, ydot_naught, yaw_rate);
+model.setPlanarLandscape(road_grade);
+
+n=0;
+e=0;
+d=0;
+
+F_brake=-100;
+
+model.setCD(.3);
+for j=1:1000;
+    model.stepSim(.01, 2, F_brake);
+    [~,~,n(end+1),e(end+1),d(end+1)]=model.getGlobalState;
+end
+
+figure; 
+plot3(n, e, d);
+xlabel('NORTH');
+ylabel('EAST');
+zlabel('DOWN');
 %% Simulating driving/turning on random surface
 tsize=250;
 terrain=genSampleTerrain(tsize+1, tsize+1);
@@ -59,8 +56,9 @@ cg_height=.5;
 frontal_area=2;
 
 %initializing generic car model
-model=Car(road_grade,mass,terminal_velocity,xdot_naught,ydot_naught,...
-            del,yaw_rate,wheel_base_width,wheel_base_length,r_eff,c_rr,cg_height,frontal_area);
+model=Car(mass, wheel_base_width,wheel_base_length,r_eff,c_rr,cg_height,frontal_area,del);
+%setting up a simulation
+model=model.SetupSim(xdot_naught, ydot_naught, yaw_rate);
 
 x=0;
 xdot=xdot_naught;
@@ -76,7 +74,7 @@ F_brake=-100;
 model.setCD(.3);
 model.setPlanarLandscape(terrain);
 for j=1:1000;
-    model.stepTerrainBrakeSim(.1, 2, F_brake);
+    model.stepSim(.1, 2, F_brake);
     [yaw(end+1), x(end+1),xdot(end+1),~,y(end+1),ydot(end+1),~]=model.getState;
     [~,~,n(end+1),e(end+1),d(end+1)]=model.getGlobalState;
 end
@@ -86,6 +84,14 @@ e=e+tsize/2;
 figure; 
 surfl(terrain);
 hold on;
+plot3(n(2:end), e(2:end), d(2:end),'r');
+xlabel('NORTH');
+ylabel('EAST');
+zlabel('DOWN');
+xlim([0,tsize+1]);
+ylim([0,tsize+1]);
+
+figure;
 plot3(n(2:end), e(2:end), d(2:end),'r');
 xlabel('NORTH');
 ylabel('EAST');
